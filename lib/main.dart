a54b1e3bd749485a0e04fde2b2a0e760aa69436a
tanveertrios <tanveer.kaurj2k@triosstudent.com>
2023-08-01 20:43:03 -0400

    FINAL COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 
+ 
+
 void main() {
+
   runApp(const MyApp());
+
 }
 
+ 
+
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
+ 
+
   @override
+
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
       title: 'Home Page',
+
       home: Scaffold(
+
         appBar: AppBar(
+
           title: const Text('Home page'),
+
         ),
+
         body: const Center(
+
           child: Text('Hello World'),
+
         ),
+
       ),
+
     );
+
   }
+
 }
